7fa5c885e98fde68c1723b2e065389f0572f8e99
eeeaaa00 <sky05028@naver.com>
2022-02-11 23:27:08 +0900

    FIX : 페이징처리
    매직넘버를 심볼릭처리하던 중 rangeSize가 변경되었을 경우 생기는 오류 수정

--- src/main/java/com/dwu/alonealong/domain/FoodFunction.java
+++ src/main/java/com/dwu/alonealong/domain/FoodFunction.java
@@ -18,26 +18,32 @@ public class FoodFunction {
 	public static final int defaultInt = 0;
 	
 	public static void pagingRestaurantList(List<Restaurant> restaurantList, ModelMap model, int page) {
+		int itemsForPage = 6;
+		int rangeSize = 3;
 		PagedListHolder<Restaurant> pagedRestaurantList = new PagedListHolder<Restaurant>(restaurantList);
-        pagedRestaurantList.setPageSize(6);
-        pagedRestaurantList.setPage(page - 1);
+        pagedRestaurantList.setPageSize(itemsForPage); //페이지당 보여줄 개수
+        pagedRestaurantList.setPage(page - 1); //보여줄 페이지
         model.put("restaurantList", pagedRestaurantList.getPageList());
 		model.put("restaurantCount", restaurantList.size());
 		
 		model.put("page", pagedRestaurantList.getPage() + 1);
-		model.put("startPage", (pagedRestaurantList.getPage() / 5) * 5 + 1);
+		model.put("startPage", (pagedRestaurantList.getPage() / rangeSize) * rangeSize + 1);
 		model.put("lastPage", pagedRestaurantList.getPageCount());
+		model.put("rangeSize", rangeSize);
 	}
 	public static void pagingReviewList(List<FoodReview> reviewList, ModelMap model, int page) {
+		int itemsForPage = 3;
+		int rangeSize = 3;
 		PagedListHolder<FoodReview> pagedReviewList = new PagedListHolder<FoodReview>(reviewList);
-	    pagedReviewList.setPageSize(6);
+	    pagedReviewList.setPageSize(itemsForPage);
 	    pagedReviewList.setPage(page - 1);
 	    model.put("foodReviewList", pagedReviewList.getPageList());
 		model.put("foodReviewCount", reviewList.size());
 		
 		model.put("page", pagedReviewList.getPage() + 1);
-		model.put("startPage", (pagedReviewList.getPage() / 5) * 5 + 1);
+		model.put("startPage", (pagedReviewList.getPage() / rangeSize) * rangeSize + 1);
 		model.put("lastPage", pagedReviewList.getPageCount());
+		model.put("rangeSize", rangeSize);
 	}
 	public static void encodeImgList(List<?> list){
 		Encoder encoder = Base64.getEncoder();
