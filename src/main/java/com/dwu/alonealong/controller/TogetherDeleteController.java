71cc3a11d3e0f4d59565763ceda72371750d3ecb
rin-k645 <mwtgal20@gmail.com>
2022-03-11 00:18:33 +0900

    FEAT: REST API - deleteTogether

--- src/main/java/com/dwu/alonealong/controller/TogetherDeleteController.java
+++ src/main/java/com/dwu/alonealong/controller/TogetherDeleteController.java
@@ -1,20 +1,13 @@
 package com.dwu.alonealong.controller;
 
-import java.util.List;
-
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import com.dwu.alonealong.domain.Order;
-import com.dwu.alonealong.domain.Together;
 import com.dwu.alonealong.service.AloneAlongFacade;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class TogetherDeleteController {
 	private AloneAlongFacade aloneAlong;
 	
@@ -22,12 +15,9 @@ public class TogetherDeleteController {
 	public void setAlonealong(AloneAlongFacade aloneAlong) {
 		this.aloneAlong = aloneAlong;
 	}
-	
-	@GetMapping("/togetherRegister/delete/{togetherId}")
-	public String deleteTogether(
-			HttpServletRequest request,
-			@PathVariable("togetherId") String togId,
-			ModelMap model) {
+
+	@DeleteMapping("/together/delete/{togetherId}")
+	public void deleteTogether(@PathVariable("togetherId") String togId) {
 		String orderId = aloneAlong.getTogetherOrderByTogId(togId).get(0).getOrderId();
 
 		aloneAlong.deleteTogetherFood(togId); //음식 리스트 삭제
@@ -35,11 +25,6 @@ public class TogetherDeleteController {
 		aloneAlong.deleteTogetherOrder(togId); //TogetherOrder 삭제
 		aloneAlong.deleteTogetherOrderInfo(orderId); //OrderInfo 삭제
 		aloneAlong.deleteTogether(togId); //together 삭제
-		
-		List<Together> togetherList = aloneAlong.getTogetherList(); //JPA 여기서 에러남
-		model.addAttribute("togetherList", togetherList);
-		
-		return "redirect:/together";
 	}
 	
 }
